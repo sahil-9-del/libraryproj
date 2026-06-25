f2006e2205bed172c7a4ebebd8495409d06d6187
sahilpalme <sahilpal.me@gmail.com>
2026-06-25 18:56:37 +0530

    my first commit

---
+++ book.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+struct book_details
+{
+    int Book_id;
+    char author[64];
+    char title[64];
+    char category[21];
+    int total_quantity;
+};
+
+int main()
+{
+    struct book_details d;
+    
+    
+    printf("Enter Author: ");
+    fgets(d.author,sizeof(d.author),stdin);
+    printf("Enter Title: ");
+    fgets(d.title,sizeof(d.title),stdin);
+    printf("Enter Category: ");
+    fgets(d.category,sizeof(d.category),stdin);
+    printf("Enter Book_id: ");
+    scanf("%d",&d.Book_id);
+    printf("Enter total_quantity");
+    scanf("%d",&d.total_quantity);
+
+    
+   
+        
+    
+    
+    
+    
+    return 0;
+}

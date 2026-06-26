73b3291e07bc7f2575221522cb064a7c69d234aa
sahilpalme <sahilpal.me@gmail.com>
2026-06-26 23:38:11 +0530

    DAY 2 ENDED

--- book.c
+++ book.c
@@ -11,31 +11,77 @@ struct book_details
 };
 
 int main()
-{
-    struct book_details d;
-
-    
-    
-    printf("Enter Author: ");
-    gets(d.author);
-    printf("Enter Title: ");
-    gets(d.title);
-    printf("Enter Category: ");
-    gets(d.category);
-    printf("Enter Book_id: ");
-    scanf("%d",&d.Book_id);
-    printf("Enter total_quantity: ");
-    scanf("%d",&d.total_quantity);
-
-    FILE* ptr = NULL;
-    ptr = fopen("book.txt","a");
-    
-    fprintf(ptr,"%s %s %s %d %d",d.author,d.title,d.category,d.Book_id,d.total_quantity);
-    printf("\n");
-
-    fclose(ptr);
-   
+{   struct book_details d;
+    int a;
+    printf("Enter 1 to add book details\nEnter 2 to search a book\n");
+    scanf("%d",&a);
+    getchar();
+    if (a==1)
+    {
         
+        printf("Enter Author: ");
+        gets(d.author);
+        printf("Enter Title: ");
+        gets(d.title);
+        printf("Enter Category: ");
+        gets(d.category);
+        printf("Enter Book_id: ");
+        scanf("%d",&d.Book_id);
+        printf("Enter total_quantity: ");
+        scanf("%d",&d.total_quantity);
+
+        FILE* ptr = fopen("data.bin", "ab");
+        if (ptr==NULL)
+        {
+            printf("File not found");
+        }
+        fwrite(&d,sizeof(struct book_details),1,ptr);
+        fclose(ptr);    
+    }
+    else if (a==2)
+    {
+        int book_id;
+        printf("Enter the book id");
+        scanf("%d",&book_id);
+        struct book_details s1;
+         FILE* ptr = fopen("data.bin", "rb");
+        if (ptr==NULL)
+        {
+            printf("File not found");
+        }
+        fread(&s1,sizeof(struct book_details),1,ptr);
+        while (book_id!=s1.Book_id)
+        {
+            
+            if (s1.Book_id == book_id)
+            {
+                printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %stotal quantity: %d",s1.Book_id,s1.author,s1.title,s1.category,s1.total_quantity);
+            }
+            
+        }
+        fclose(ptr);
+        
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+
+   
+   
+    
     
     
     

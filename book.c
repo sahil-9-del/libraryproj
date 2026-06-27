d4f6f5027ffc04b14b3f99fbd8db2cd1047fcfca
sahilpalme <sahilpal.me@gmail.com>
2026-06-27 11:24:22 +0530

    solve to probem with searching

--- book.c
+++ book.c
@@ -11,14 +11,15 @@ struct book_details
 };
 
 int main()
-{   struct book_details d;
+{
+    struct book_details d;
     int a;
     printf("Enter 1 to add book details\nEnter 2 to search a book\n");
-    scanf("%d",&a);
+    scanf("%d", &a);
     getchar();
-    if (a==1)
+    if (a == 1)
     {
-        
+
         printf("Enter Author: ");
         gets(d.author);
         printf("Enter Title: ");
@@ -26,66 +27,52 @@ int main()
         printf("Enter Category: ");
         gets(d.category);
         printf("Enter Book_id: ");
-        scanf("%d",&d.Book_id);
+        scanf("%d", &d.Book_id);
         printf("Enter total_quantity: ");
-        scanf("%d",&d.total_quantity);
+        scanf("%d", &d.total_quantity);
 
-        FILE* ptr = fopen("data.bin", "ab");
-        if (ptr==NULL)
+        FILE *ptr = fopen("data.bin", "ab");
+        if (ptr == NULL)
         {
             printf("File not found");
         }
-        fwrite(&d,sizeof(struct book_details),1,ptr);
-        fclose(ptr);    
+        fwrite(&d, sizeof(struct book_details), 1, ptr);
+        fclose(ptr);
     }
-    else if (a==2)
+    else if (a == 2)
     {
         int book_id;
-        printf("Enter the book id");
-        scanf("%d",&book_id);
+        printf("Enter the book id:\n");
+        scanf("%d", &book_id);
         struct book_details s1;
-         FILE* ptr = fopen("data.bin", "rb");
-        if (ptr==NULL)
+        FILE *ptr = fopen("data.bin", "rb");
+        if (ptr == NULL)
         {
             printf("File not found");
+            return 0;
         }
-        fread(&s1,sizeof(struct book_details),1,ptr);
-        while (book_id!=s1.Book_id)
+
+        while (fread(&s1, sizeof(struct book_details), 1, ptr))
         {
-            
+
             if (s1.Book_id == book_id)
-            
+
             {
-                printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %stotal quantity: %d",s1.Book_id,s1.author,s1.title,s1.category,s1.total_quantity);
+                break;
             }
-            
-        }
-        fclose(ptr);
-        
+        }    
 
+            if (s1.Book_id == book_id)
+            {
+                printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %stotal quantity: %d", s1.Book_id, s1.author, s1.title, s1.category, s1.total_quantity);
+            }
+            else
+            {
+                printf("Book not found");
+            }
+
+            fclose(ptr);
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-    
-    
-    
-    
-    
-
-   
-   
-    
-    
-    
-    
-    
-    return 0;
-}
+        return 0;
+    }

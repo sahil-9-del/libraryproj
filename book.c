c1faa15558bc000be51da49ea9d0c4cfbe4d15ed
sahilpalme <sahilpal.me@gmail.com>
2026-06-28 21:30:18 +0530

    convert the main into functions and working on issuebook

--- book.c
+++ book.c
@@ -8,38 +8,32 @@ struct book_details
     char title[64];
     char category[21];
     int total_quantity;
+    int available_quantity;
 };
-
-int main()
+struct book_details d;
+void addbook() //addbook()
 {
-    struct book_details d;
-    int a;
-    printf("Enter 1 to add book details\nEnter 2 to search a book\nEnter 3 to display all books\n");
-    scanf("%d", &a);
-    getchar();
-    if (a == 1)
+    printf("Enter Author: ");
+    gets(d.author);
+    printf("Enter Title: ");
+    gets(d.title);
+    printf("Enter Category: ");
+    gets(d.category);
+    printf("Enter Book_id: ");
+    scanf("%d", &d.Book_id);
+    printf("Enter total_quantity: ");
+    scanf("%d", &d.total_quantity);
+    d.available_quantity=d.total_quantity;
+
+    FILE *ptr = fopen("data.bin", "ab");
+    if (ptr == NULL)
     {
-
-        printf("Enter Author: ");
-        gets(d.author);
-        printf("Enter Title: ");
-        gets(d.title);
-        printf("Enter Category: ");
-        gets(d.category);
-        printf("Enter Book_id: ");
-        scanf("%d", &d.Book_id);
-        printf("Enter total_quantity: ");
-        scanf("%d", &d.total_quantity);
-
-        FILE *ptr = fopen("data.bin", "ab");
-        if (ptr == NULL)
-        {
-            printf("File not found");
-        }
-        fwrite(&d, sizeof(struct book_details), 1, ptr);
-        fclose(ptr);
+        printf("File not found");
     }
-    else if (a == 2)
+    fwrite(&d, sizeof(struct book_details), 1, ptr);
+    fclose(ptr);
+}    
+    void searchbook() //searchbook()
     {
         int book_id;
         printf("Enter the book id:\n");
@@ -49,7 +43,7 @@ int main()
         if (ptr == NULL)
         {
             printf("File not found");
-            return 0;
+            return;
         }
 
         while (fread(&s1, sizeof(struct book_details), 1, ptr))
@@ -64,7 +58,7 @@ int main()
 
         if (s1.Book_id == book_id)
         {
-            printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %s\ntotal quantity: %d", s1.Book_id, s1.author, s1.title, s1.category, s1.total_quantity);
+            printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %s\ntotal quantity: %d\nAvailable quantity: %d", s1.Book_id, s1.author, s1.title, s1.category, s1.total_quantity,s1.available_quantity);
         }
         else
         {
@@ -73,19 +67,19 @@ int main()
 
         fclose(ptr);
     }
-    else if (a == 3)
+    void displaybook() // display book()
     {
         struct book_details s1;
         FILE *ptr = fopen("data.bin", "rb");
         if (ptr == NULL)
         {
             printf("File not found");
-            return 0;
+            return;
         }
         int count = 0;
         while (fread(&s1, sizeof(struct book_details), 1, ptr))
         {
-            printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %s\ntotal quantity: %d\n", s1.Book_id, s1.author, s1.title, s1.category, s1.total_quantity);
+            printf("book id: %d\nAuthor: %s\nTitle: %s\nCategory: %s\ntotal quantity: %d\nAvailable quantity: %d\n", s1.Book_id, s1.author, s1.title, s1.category, s1.total_quantity,s1.available_quantity);
             count++;
             printf("---------------------------\n");
         }
@@ -93,7 +87,51 @@ int main()
         {
             printf("The library was empty");
         }
+        fclose(ptr);
     }
+    void issuebook()
+    {
+        int a,b;
+        printf("Enter the book id");
+        scanf("%d",&a);
+        printf("Enter the student id");
+        scanf("%d",&b);
+        struct book_details s2;
+        FILE*ptr = fopen("data.bin", "rb+");
+        if (ptr==NULL)
+        {
+            printf("File not found");
+            return;
+        }
+        while (fread(&s2, sizeof(struct book_details), 1, ptr))
+        {
 
-    return 0;
-}
+            if (s2.Book_id == a)
+
+            {
+                if (s2.available_quantity>0)
+                {
+                    s2.available_quantity--;
+                    break;
+                }
+                else{
+                    printf("Book is not available");
+                    break;
+                }
+            }
+        if (s2.Book_id==a)
+        {
+            
+        }
+        else{
+            printf("Book not found");
+        }
+            
+            
+        }
+        
+    }
+    
+
+    
+
